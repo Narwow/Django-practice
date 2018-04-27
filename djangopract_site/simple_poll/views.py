3f216b10a90affc02863df1a8d6b8929268fb91a
Jhuo Wei Ku <jhuoweiku@gmail.com>
2018-04-27 11:20:38 -0700

    add other views

--- djangopract_site/simple_poll/views.py
+++ djangopract_site/simple_poll/views.py
@@ -7,4 +7,13 @@ from django.http import HttpResponse
 # Simple http response
 def index(request):
     return HttpResponse("This is a simple response at simple_poll index")
-    
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." %question_id)
+
+def results(request, question_id):
+    response = "You are looking at the results of questions %s."
+    return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." %question_id)    
