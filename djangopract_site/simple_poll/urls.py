627383d6f765aa3e12b04866de2e64d9a6dd57f6
Jhuo Wei Ku <jhuoweiku@gmail.com>
2018-05-03 16:43:02 -0700

    remove hardcoded path and add namespace

--- djangopract_site/simple_poll/urls.py
+++ djangopract_site/simple_poll/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib import admin
 from . import views
 
+app_name = 'simple_poll'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
